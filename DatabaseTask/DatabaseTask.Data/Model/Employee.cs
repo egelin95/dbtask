527603cc3fdbc40f1422e704fd46e1ed678c3471
egg <egelin.ellermaa@gmail.com>
2024-05-18 15:08:00 +0300

    db first models generated based on existing tables

---
+++ DatabaseTask/DatabaseTask.Data/Model/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseTask.Data.Model
+{
+    public partial class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
